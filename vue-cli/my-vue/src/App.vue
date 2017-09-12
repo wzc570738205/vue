921f46f2bbc6615ceccb45949ecabb395231a3e6
wangzhichao570738205 <15210375449@163.com>
2017-09-12 10:58:32 +0800

    sd

---
+++ vue-cli/my-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <img src="./assets/logo.png">
+   
+  </div>
+</template>
+
+<script>
+import Header from "./components/header.vue";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    "v-header":Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/my-vue/src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="header">
+		{{message}}
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		data(){
+			return {
+				message:"this is headerhaha"
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	.header{
+		width: 100%;
+		height: 50px;
+		line-height: 50px;
+		text-align: center;
+		background: #000;
+		color: #fff;
+	}
+</style>
